b17207d5a57ee134ecb1728649aa0f4d17faee61
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-13 16:07:37 -0500

    Practice with list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import ipdb
 
 def return_evens(num_list):
-    pass
 
+    even_list = [n for n in num_list if (n % 2 == 0)]
+    return even_list
+    
 def make_exclamation(sentence_list):
-    pass
+    
+    return [n + "!" for n in sentence_list]
